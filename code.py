a0fc0c70826a3bdbb97d68f8ae6366f0fade4da4
Evan Sharp <ego@evansharp.ca>
2018-02-05 12:29:12 -0800

    Create code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+#Example
+swiftyVar = "Mr. Sharp's Example String Var"
+print( swiftyVar )
+
+
+#You Try: 
+#define a String
+
+
+#define an Integer
+
+
+#define a float
+
+
+#print your vars (did you see the *bonus* !?!)
